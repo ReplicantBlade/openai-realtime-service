fd7566176a239e6c057f0268e36d092c207c64c8
Replicant <MehdiRabieiRep@protonmail.com>
2024-10-07 00:48:09 +0330

    update

--- src/OpenAiService.js
+++ src/OpenAiService.js
@@ -1,9 +1,8 @@
 // src/services/openAiService.js
 import {RealtimeClient} from '@openai/realtime-api-beta';
-import fs from "fs";
-import path from "path";
 
 export class OpenAiService {
+
     constructor(apiKey) {
         this.client = new RealtimeClient({apiKey});
     }
@@ -22,55 +21,7 @@ export class OpenAiService {
         }
     }
 
-    ids = new Set();
-
-    async sendVoiceMessage(audio, callback) {
-        try {
-            this.client.sendUserMessageContent([{type: 'input_audio', audio}]);
-
-            this.client.on('conversation.item.completed', async ({item}) => {
-                if (item.type === 'message') {
-                    const items = this.client.conversation.getItems();
-                    const modifiedItems = [];
-
-                    for (const item of items) {
-                        const fileName = `${item.id}.wav`;
-
-                        if (this.ids.has(fileName) || item.role !== "assistant") continue;
-
-                        const base64Audio = int16ArrayToBase64(item.formatted.audio);
-
-                        // fs.existsSync(path.join('/mnt/public_files/files', fileName))
-                        //const audioFilePath = await saveAudioToFile(base64Audio, fileName);
-                        //item.formatted["audioDownloadLink"] = `${process.env.SERVER_ADDR}/${path.basename(audioFilePath)}`;
-                        this.ids.add(fileName);
-                        item.formatted["audioBase64"] = base64Audio;
-
-                        modifiedItems.push(item);
-                    }
-
-                    if (modifiedItems.length === 0) return;
-
-                    callback(modifiedItems);
-                }
-            });
-        } catch (error) {
-            console.error('Error sending voice message:', error);
-        }
-
-        function int16ArrayToBase64(int16Array) {
-            const buffer = Buffer.from(int16Array.buffer);
-            return buffer.toString('base64');
-        }
-
-        async function saveAudioToFile(base64Audio, fileName) {
-            const filePath = path.join('/mnt/public_files/files', fileName); // Change the directory
-
-            const audioBuffer = Buffer.from(base64Audio, 'base64');
-            await fs.promises.writeFile(filePath, audioBuffer);
-
-            return filePath;
-        }
+    getClient() {
+        return this.client;
     }
-
 }
--- src/SocketIO.js
+++ src/SocketIO.js
@@ -1,6 +1,9 @@
 import * as socketIO from 'socket.io';
+import fs from "fs";
+import path from "path";
 
 export function setupSocketIO(server, openAiService) {
+
     const io = new socketIO.Server(server, {
         allowUpgrades: true,
         transports: ["websocket", "polling"],
@@ -9,18 +12,73 @@ export function setupSocketIO(server, openAiService) {
         maxHttpBufferSize: 1e7
     });
 
-    io.on('connection',  async (socket) => {
-        socket.on('VoiceMessage', async (audio) => {
-            await openAiService.sendVoiceMessage(audio, async (response) => {
-                console.log("Sending response")
-                await socket.emitWithAck("VoiceResponse", response);
-            });
+    io.on('connection', async (socket) => {
+
+        socket.on('StreamVoiceChunk', async (data) => {
+
+            const audioData = new Int16Array(data.buffer, data.byteOffset, data.byteLength / Int16Array.BYTES_PER_ELEMENT);
+            openAiService.getClient().appendInputAudio(audioData);
+
         });
 
-        socket.on('disconnect', async (socket) =>{
-            console.error('A user disconneced:', socket);
+        socket.on('StreamVoiceEnd', async () => {
+
+            openAiService.getClient().createResponse();
+
+        });
+
+        openAiService.getClient().on('conversation.item.completed', async ({item}) => {
+
+            if (item.role !== "assistant") return;
+
+            // await socket.emitWithAck("VoiceResponse", {
+            //     id: item.id,
+            //     role: item.role,
+            //     audio: JSON.parse(item.formatted.audio),
+            //     text: item.formatted.text,
+            //     transcript: item.formatted.transcript,
+            // });
+
+            const audioData = item.formatted.audio;
+
+            const chunkSize = 512;
+            let responseOrder = 1;
+            for (let i = 0; i < audioData.length; i += chunkSize) {
+                const chunk = audioData.slice(i, i + chunkSize);
+                await socket.emitWithAck('AIResponseAudioChunk', {
+                    id: item.id,
+                    order: responseOrder++,
+                    audio: JSON.stringify(chunk),
+                });
+            }
+
+            await socket.emitWithAck("AIResponseComplete", {
+                id: item.id,
+                role: item.role,
+                text: item.formatted.text,
+                transcript: item.formatted.transcript,
+            });
+
+        });
+
+        socket.on('disconnect', async (socket) => {
+            console.error('A user disconnected:', socket);
         });
     });
+
+    async function saveAudioToFile(base64Audio, fileName) {
+
+        //fs.existsSync(path.join('/mnt/public_files/files', fileName))
+        //const audioFilePath = await saveAudioToFile(base64Audio, fileName);
+        //item.formatted["audioDownloadLink"] = `${process.env.SERVER_ADDR}/${path.basename(audioFilePath)}`;
+
+        const filePath = path.join('/mnt/public_files/files', fileName);
+
+        const audioBuffer = Buffer.from(base64Audio, 'base64');
+        await fs.promises.writeFile(filePath, audioBuffer);
+
+        return filePath;
+    }
 
     return io;
 }

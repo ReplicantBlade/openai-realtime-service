832c8f02d1d2edce8df413f4f0686b02b701f6b7
Replicant <MehdiRabieiRep@protonmail.com>
2024-10-10 14:23:58 +0330

    update

--- src/SocketIO.js
+++ src/SocketIO.js
@@ -105,7 +105,7 @@ export function setupSocketIO(server, openAiService) {
             console.log('A user disconnected', e);
         });
 
-        socket.emit("ConnectionHasBeenReset");
+        socket.timeout(1500).emit("ConnectionHasBeenReset");
         console.log("new client connected");
     });
 
